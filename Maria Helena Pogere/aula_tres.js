ac13e054c8995baee876761b38ca2ba3a9ddc833
maahlimfa <mariahelenapogere@gmail.com>
2026-03-09 11:03:46 -0300

    Add files via upload

---
+++ Maria Helena Pogere/aula_tres.js
@@ -0,0 +1,4 @@
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+} //o "for" indica um laço de repetição, ou seja, para os numeros ate 20 aparecerem foi necessario criar um laço de repetição
---
+++ Maria Helena Pogere/aula_um.js
@@ -0,0 +1,8 @@
+
+const input = require('prompt-sync')();
+
+
+let nome = input("Digite seu nome: ");
+let sobrenome = input("Digite seu sobrenome: ");
+
+console.log("Seu nome completo é: " + nome + " " + sobrenome)
